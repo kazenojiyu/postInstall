f7b704fed9b8583973f8e2d3cadbd3730da2489d
Sébastien Gandossi <sebastien.gandossi@gmail.com>
2016-06-20 14:24:53 -0400

    Update dump_db.sh

--- dump_db.sh
+++ dump_db.sh
@@ -14,9 +14,9 @@ date=$(date +"%d-%b-%Y")
 umask 177
 
 # Dump database into SQL file
-mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
+/usr/bin/mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
  
 echo "Dump created : $backup_path/$db_name-$date.sql"
  
 # Delete files older than 30 days
-find $backup_path/* -mtime +30 -exec rm {} \;
+/usr/bin/find $backup_path/* -mtime +30 -exec rm {} \;
